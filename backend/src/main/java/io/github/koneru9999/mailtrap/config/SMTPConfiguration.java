dc9b379dd8db54be3793372d3f06bc576446f8ca
Venkaiah Chowdary Koneru <koneru.chowdary@gmail.com>
2018-05-02 23:19:51 +0400

    check for data size before sending dummy emails

--- backend/src/main/java/io/github/koneru9999/mailtrap/config/SMTPConfiguration.java
+++ backend/src/main/java/io/github/koneru9999/mailtrap/config/SMTPConfiguration.java
@@ -2,18 +2,26 @@ package io.github.koneru9999.mailtrap.config;
 
 import com.icegreen.greenmail.util.GreenMail;
 import com.icegreen.greenmail.util.GreenMailUtil;
-import com.icegreen.greenmail.util.ServerSetup;
 import com.icegreen.greenmail.util.ServerSetupTest;
+import io.github.koneru9999.mailtrap.properties.MailSinkProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+
+import static java.time.temporal.ChronoUnit.MILLIS;
+
 @Configuration
 @Slf4j
 public class SMTPConfiguration implements ApplicationRunner {
+
+    @Autowired
+    private MailSinkProperties mailSinkProperties;
 
     @Bean(destroyMethod = "stop")
     public GreenMail greenMail() {
@@ -25,14 +33,17 @@ public class SMTPConfiguration implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        String mailBody = "<html><body><h1>Test body<h1><br>Hello user. <span>abcdefgh</span></body></html>";
-        Flux.range(0, 57)
-                .map((x) -> {
-                    GreenMailUtil.sendMessageBody("receiver@there.com", "sender@here.com",
-                            "Test" + (x + 1), mailBody, "text/html", ServerSetupTest.SMTP);
-                    return x;
-                })
-                .doOnComplete(() -> log.info("Initial Data completed"))
-                .subscribe();
+        if (mailSinkProperties.getDummyDataSize() > 0) {
+            String mailBody = "<html><body><h1>Test body<h1><br>Hello user. <span>abcdefgh</span></body></html>";
+            Flux.range(0, mailSinkProperties.getDummyDataSize())
+                    .map((x) -> {
+                        GreenMailUtil.sendMessageBody("receiver@there.com", "sender@here.com",
+                                "Test" + (x + 1), mailBody, "text/html", ServerSetupTest.SMTP);
+                        return x;
+                    })
+                    .delayElements(Duration.of(200, MILLIS))
+                    .doOnComplete(() -> log.info("Initial Data completed"))
+                    .subscribe();
+        }
     }
 }
